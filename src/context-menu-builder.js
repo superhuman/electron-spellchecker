76613048c2d654fe67a282e0babe80ae84c182f3
Islam Sharabash <islam.sharabash@gmail.com>
2018-09-16 23:18:37 -0700

    Fix copying of email address in mailto link
    Previously:
    When the link text was "foo" and the link url was "mailto:foo@bar.com".
    Copying the email address would add "foo" to the clipboard.
    
    Now:
    Copying the email address adds "foo@bar.com" to the clipboard.

--- src/context-menu-builder.js
+++ src/context-menu-builder.js
@@ -156,9 +156,12 @@ export default class ContextMenuBuilder {
     let copyLink = new MenuItem({
       label: isEmailAddress ? this.stringTable.copyMail() : this.stringTable.copyLinkUrl(),
       click: () => {
-        // Omit the mailto: portion of the link; we just want the address
-        clipboard.writeText(isEmailAddress ?
-          menuInfo.linkText : menuInfo.linkURL);
+        let url = menuInfo.linkURL;
+        if (isEmailAddress) {
+          // Omit the mailto: portion of the link; we just want the address
+          url = url.slice('mailto:'.length);
+        }
+        clipboard.writeText(url);
       }
     });
 
